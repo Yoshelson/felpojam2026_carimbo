956caf47dd3a4069d615f52167f54c53b0f801b6
RenattoPadilha <55454860+RenattoPadilha@users.noreply.github.com>
2026-02-27 10:42:38 -0300

    feat: add document board max size

--- scripts/interactable/DocumentInteractable.gd
+++ scripts/interactable/DocumentInteractable.gd
@@ -3,6 +3,7 @@ class_name DocumentInteractable
 
 var _doc_id: String
 @onready var sprite: Sprite3D = $Sprite3D
+var max_world_size: float = 0.8
 
 func _ready() -> void:
 	_id = "Document " + _doc_id
@@ -18,3 +19,9 @@ func set_id(doc_id: String):
 
 func set_image(new_texture: Texture2D):
 	sprite.texture = new_texture
+	
+	#Isso daq garante que a imagem nao cresca descontroladamente, ela eh limitada
+	#pelo max_world_size definido nessa classe
+	var texture_size = new_texture.get_size()
+	var max_pixel_dimension = max(texture_size.x, texture_size.y)
+	sprite.pixel_size = max_world_size/max_pixel_dimension
