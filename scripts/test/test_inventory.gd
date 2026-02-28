68bd17f428df6c287cb557d6e106e0dd9bee2a9e
RenattoPadilha <55454860+RenattoPadilha@users.noreply.github.com>
2026-02-27 23:05:24 -0300

    feat: stamp visual update and animation

--- scripts/test/test_inventory.gd
+++ scripts/test/test_inventory.gd
@@ -5,13 +5,14 @@ var number = 0;
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_MIDDLE:
-			#Test ADD e Signal p/ UI
 			var load_doc1 = load("res://resources/documents/Autopsia.tres")
 			var load_doc2 = load("res://resources/documents/Ficha_Criminal.tres")
-			var load_doc3 = load("res://resources/documents/test_doc_3.tres")
+			var load_doc3 = load("res://resources/documents/Relatorio_Homicidio.tres")
+			var load_doc4 = load("res://resources/documents/Jornal.tres")
 			InventoryManager.add_document(load_doc1)
 			InventoryManager.add_document(load_doc2)
 			InventoryManager.add_document(load_doc3)
+			InventoryManager.add_document(load_doc4)
 
 			var load_stamp1 = load("res://resources/stamps/BlackStamp.tres")
 			var load_stamp2 = load("res://resources/stamps/WhiteStamp.tres")
@@ -23,8 +24,4 @@ func _input(event: InputEvent) -> void:
 			InventoryManager.add_stamp_color(load_stamp3)
 			InventoryManager.add_stamp_color(load_stamp4)
 			InventoryManager.add_stamp_color(load_stamp5)
-		elif event.button_index == MOUSE_BUTTON_RIGHT:
-			#Testar Signal e GetDoc e UI
-			var document_UI = $".."
-			document_UI.setup_UI("ficha_criminal")
 		
